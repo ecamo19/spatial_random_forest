abeab7f522f744ce968fd97770029af8d90bb0c2
ecamo19 <erickcamo19@gmail.com>
2022-12-04 19:28:21 -0500

    Script created for automating analisys

---
+++ scripts/run_all_analysis.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# Get spatial params gpgp
+
+# Run Spatial random forest for Redundancy and for FDis
+Rscript model_fitting 
+
+# Run spcopula model using fitted values 
+
+# Run Spatial random forest variable importance
+
+# Run Spatial random forest shapley values
+

8c3fe512b5c7b325338c0e1c8cdc9d8c082193b8
ecamo19 <erickcamo19@gmail.com>
2022-12-23 00:30:32 -0500

    File updated

--- Snakefile
+++ Snakefile
@@ -2,6 +2,7 @@ rule targets:
         input:
                 "data/cleaned_data/original_species_list.csv",
                 "data/cleaned_data/tnrs_names.csv",
+                "data/cleaned_data/species_list_updated.csv",
                 "data/raw_data/raw_salgado_original.xls",
                 "data/raw_data/raw_chazdon_2003.html",
                 "data/raw_data/raw_nzamora_original.xlsx",
@@ -22,13 +23,14 @@ rule get_original_species_list:
                         --out {output.data}
                 """
 
-rule get_updated_species_names:
+rule get_tnrs_and_species_list_updated:
         input:
-                script = "scripts/script_get_tnrs_names.R",
+                script = "scripts/script_get_tnrs_and_species_list_updated.R",
                 data = "data/cleaned_data/original_species_list.csv"
         output:
                 data_1 = "data/cleaned_data/tnrs_names.csv",
-                data_2 = "data/cleaned_data/updated_species_list.csv"
+                data_2 = "data/cleaned_data/species_list_updated.csv"
+
         shell:
                 """
                 Rscript {input.script} \
@@ -40,12 +42,11 @@ rule get_updated_species_names:
 rule create_traits_db:
         input:
                 script = "scripts/script_trait_db_construction.R",
-                data_1 = "data/cleaned_data/species_list.csv",
-                data_2 = "data/cleaned_data/tnrs_names.csv",
-                data_3 = "data/raw_data/raw_salgado_original.xls",
-                data_4 = "data/raw_data/raw_chazdon_2003.html",
-                data_5 = "data/raw_data/raw_nzamora_original.xlsx",
-                data_6 = "data/raw_data/raw_data_vargas.RData"
+                data_1 = "data/cleaned_data/species_list_updated.csv",
+                data_2 = "data/raw_data/raw_salgado_original.xls",
+                data_3 = "data/raw_data/raw_chazdon_2003.html",
+                data_4 = "data/raw_data/raw_nzamora_original.xlsx",
+                data_5 = "data/raw_data/raw_data_vargas.RData"
         output:
                 data = "data/cleaned_data/db_traits_190.csv"
         shell:
@@ -56,7 +57,6 @@ rule create_traits_db:
                         --data {input.data_3} \
                         --data {input.data_4} \
                         --data {input.data_5} \
-                        --data {input.data_6} \
                         --out {output.data}
                 """
 
--- scripts/script_get_agb_data.R
+++ scripts/script_get_agb_data.R
@@ -1,4 +1,7 @@
 # Objetive ---------------------------------------------------------------------
+# Estimate the abovegorund biomass of each plot using the biomass R package
+
+here::i_am("data_analysis/scripts/script_get_agb_data.R")
 
 # Load packages ----------------------------------------------------------------
 library(dplyr)
@@ -7,59 +10,41 @@ library(janitor)
 library(tibble)
 # For pivot
 library(tidyr)
-
-# Objetive ---------------------------------------------------------------------
-# Estimate the abovegorund biomass of each plot using the biomass R package
-
-# Load packages ----------------------------------------------------------------
+# For calculate Aboveground Biomass
 library(BIOMASS)
-library(dplyr)
+# For reading .xlsx file
 library(readxl)
 
 # Load data --------------------------------------------------------------------
 basal_area_wide <-
-    read_excel('./data/raw_data/abundancia_aboveground_biomass_data/data_basal_area_sp_parcela.xlsx', sheet = 2)
+    read_excel("./data/raw_data/raw_species_basal_area.xlsx", sheet = 2)  %>%
 
-
-
-
-
-
-
-# Load raw data ----------------------------------------------------------------
-
-## AGB data --------------------------------------------------------------------
-raw_agb_data <-
-    readxl::read_xlsx("./raw_data/abundancia_aboveground_biomass_data/data_AB_sp_parcela.xlsx",
-                      sheet = 2) %>%
-
-        # Remove last row
+        # Remove last row, for some reason read_excel imports a row filled with
+        # NAs
         slice(1:127) %>%
         clean_names() %>%
 
         # Remove species
-        select(-c("brospa", "hirtme", "ruptca","quetoc", "maytgu" ))
+        select(-c("brospa", "hirtme", "ruptca", "quetoc", "maytgu"))
 
-## Read trait data to get new spcodes ------------------------------------------
-traits <-
-    read.csv("./data_for_analisys/traits_data.csv", header = T) %>%
+
+## Read species list ----------------------------------------------------------
+species_list_257 <-
+    read.csv("./data/cleaned_data/species_list.csv", header = TRUE) %>%
         select(-X)
 
-### Get spcodes from trait dataset ---------------------------------------------
-spcodes_traits_only <-
-    traits %>%
-        select(1:3) %>%
-        arrange(spcode)
 
 # Clean data -------------------------------------------------------------------
-data_agb_long <-
-    raw_agb_data %>%
+basal_area_long <-
+    basal_area_wide %>%
+
         # Change to long format
-        pivot_longer(!parcela, names_to = "spcode", values_to = "agb")
+        pivot_longer(!parcela, names_to = "spcode",
+                        values_to = "basal_area_m2_ha")
 
 # Test -------------------------------------------------------------------------
-data_agb_long %>%
-    anti_join(.,spcodes_traits_only, by = "spcode")
+basal_area_long %>%
+    anti_join(., species_list_257, by = "spcode")
 
 ## Double check spcodes --------------------------------------------------------
 data_agb_long %>%
